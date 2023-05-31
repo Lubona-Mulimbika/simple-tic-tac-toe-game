9d6a0c730ff22ce3b9403fa7ab87a1f0cdcaf6e0
Lubona-Mulimbika <100514619+Lubona-Mulimbika@users.noreply.github.com>
2023-05-31 13:21:16 +0200

    initial tic tactoe code

---
+++ script.js
@@ -0,0 +1,59 @@
+const cells = document.querySelectorAll(".cell");
+let currentPlayer = "X";
+let isGameActive = true;
+
+const winningScenarios = [
+  // Rows
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  // Columns
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  // Diagonals
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const board = ["", "", "", "", "", "", "", "", ""];
+
+// Add click event listener to each cell
+cells.forEach((cell, index) => {
+  cell.addEventListener("click", function handleCellClick() {
+    if (this.textContent === "" && isGameActive === true) {
+      this.textContent = currentPlayer;
+      board[index] = currentPlayer;
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
+      checkWin(currentPlayer);
+    }
+  });
+});
+
+function checkWin(player) {
+  for (let i = 0; i < winningScenarios.length; i++) {
+    const [cell1, cell2, cell3] = winningScenarios[i];
+
+    if (
+      board[cell1] === player &&
+      board[cell2] === player &&
+      board[cell3] === player
+    ) {
+      alert("Player " + player + " wins!"); // Player has won
+      isGameActive = false;
+    }
+  }
+}
+
+const resetBtn = document.querySelector("#reset-button");
+
+// Add click event listener to the reset button
+resetBtn.addEventListener("click", function () {
+  // Reset the text content of each cell to an empty string and reset current player to X
+  cells.forEach(function (cell) {
+    currentPlayer = "X";
+    cell.textContent = "";
+  });
+  board.fill(""); // Clear the board array
+  isGameActive = true; // Set the game to active
+});
